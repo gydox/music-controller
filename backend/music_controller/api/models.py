9da36c0d442af57df40b16d14da175e58bff7e77
Shawn <shawntayws@gmail.com>
2024-03-15 15:41:09 +0800

    fix room model

--- backend/music_controller/api/models.py
+++ backend/music_controller/api/models.py
@@ -19,4 +19,4 @@ class Room(models.Model):
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
-    created_at = models.DateTimeField(auto_add_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
