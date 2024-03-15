64dfcad6487b20763569639e78f720dfebd9e027
Shawn <shawntayws@gmail.com>
2024-03-15 15:40:57 +0800

    created code generator for room

--- backend/music_controller/api/models.py
+++ backend/music_controller/api/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choices(string.ascii_upercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+        return code
 
 
 class Room(models.Model):
