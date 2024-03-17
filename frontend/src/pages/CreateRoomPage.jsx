7440bacbef7eb0fe15c5857b8d668c7b2d9727d7
Shawn <shawntayws@gmail.com>
2024-03-17 16:38:59 +0800

    added create room page to wrok with api endpoints at /api/create_room

--- frontend/src/pages/CreateRoomPage.jsx
+++ frontend/src/pages/CreateRoomPage.jsx
@@ -1,5 +1,106 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { useState } from "react";
 
-export default function CreateRoomPage() {
-  return <div>Create Room Page</div>;
+export default function CreateRoomPage(props) {
+  const defaultVotes = 2;
+
+  const [guestCanPause, setGuestCanPause] = useState(true);
+  const [votesToSkip, setVotesToSkip] = useState(defaultVotes);
+
+  const handleVotesChange = (e) => {
+    setVotesToSkip(e.target.value);
+    console.log("handled votes change");
+  };
+
+  const handleGuestCanPauseChange = (e) => {
+    setGuestCanPause(e.target.value === "true" ? true : false);
+  };
+
+  const handleRoomButtonPressed = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        guest_can_pause: guestCanPause,
+        votes_to_skip: votesToSkip,
+      }),
+    };
+    fetch("http://127.0.0.1:8000/api/create_room", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component="h4" variant="h4">
+          Create a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControl component="fieldset">
+          <Typography component="h5" cariant="h5">
+            Guest Control of Playback State
+          </Typography>
+          <RadioGroup
+            row
+            defaultValue="true"
+            onChange={handleGuestCanPauseChange}
+          >
+            <FormControlLabel
+              value="true"
+              control={<Radio color="primary" />}
+              label="Play/Pause"
+              labelPlacement="bottom"
+            ></FormControlLabel>
+            <FormControlLabel
+              value="false"
+              control={<Radio color="secondary" />}
+              label="No Control"
+              labelPlacement="bottom"
+            ></FormControlLabel>
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControl>
+          <TextField
+            id="votes"
+            required={true}
+            type="number"
+            onChange={handleVotesChange}
+            defaultValue={defaultVotes}
+            inputProps={{ min: 1, style: { textAlign: "center" } }}
+          />
+          <FormHelperText component={"span"}>
+            <div align="center">Votes Required to Skip Song</div>
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={handleRoomButtonPressed}
+        >
+          Create Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button color="secondary" variant="contained" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
