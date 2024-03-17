8305f6eeed2ecf6cb5e4b382d92cbfbe67d5563d
Shawn <shawntayws@gmail.com>
2024-03-17 23:15:59 +0800

    fix response code

--- backend/music_controller/api/views.py
+++ backend/music_controller/api/views.py
@@ -29,11 +29,11 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
                 room.save()
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
-            return Response(RoomSerializer(room).data, status=201)
-        else:
-            return Response(serializer.errors, status=400)
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
