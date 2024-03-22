28c1d425b0cea3636034eb58136c79cad82b23b4
Shawn <shawntaydev@gmail.com>
2024-03-23 00:04:56 +0800

    Create url path for JoinRoom view - /join_room

--- backend/music_controller/api/urls.py
+++ backend/music_controller/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create_room', CreateRoomView.as_view()),
     path('get_room', GetRoom.as_view()),
+    path('join_room', JoinRoom.as_view()),
 ]
