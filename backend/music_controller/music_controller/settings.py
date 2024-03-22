a2058421d944cb4b045807d166a41003cc71c6d6
Shawn <shawntaydev@gmail.com>
2024-03-22 14:37:59 +0800

    created GetRoom api view

--- backend/music_controller/music_controller/settings.py
+++ backend/music_controller/music_controller/settings.py
@@ -33,7 +33,14 @@ CORS_ALLOWED_ORIGINS = [
     'http://0.0.0.0',
 ]
 
-CORS_ALLOWED_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://0.0.0.0',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Application definition
 
