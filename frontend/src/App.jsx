5ff25b36f8ee613efdbdf009c2e9ff68e2265269
Shawn <shawntaydev@gmail.com>
2024-03-22 17:38:42 +0800

    Connect Room page to get_room api endpoint, sucessfully fetch data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from "./pages/HomePage";
 import axios from "axios";
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
+axios.defaults.withCredentials = true;
 
 // axios.defaults.withCredentials = true;
 
--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 export default function Room(props) {
   const defaultVotes = 2;
+  const params = useParams();
   const [votesToSkip, setVotesToSkip] = useState(defaultVotes);
   const [guestCanPause, setGuestCanPause] = useState(false);
   const [isHost, setIsHost] = useState(false);
 
-  // Get the room ID from the URL parameters.
-  const params = useParams();
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/get_room" + `?code=${params.roomCode}`)
+      .then((response) => {
+        const data = response.data; // Access response data directly
+        setVotesToSkip(data.votes_to_skip);
+        setGuestCanPause(data.guest_can_pause.toString());
+        setIsHost(data.is_host.toString());
+      })
+      .catch((error) => {
+        console.error("Error fetching room data:", error);
+      });
+  }, [params.roomCode]);
+
   return (
     <div>
       <h3>Room Code: {params.roomCode}</h3>
