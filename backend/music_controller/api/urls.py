9ba773e90517c8a0082dd6951468bd451a2ca595
Shawn <shawntaydev@gmail.com>
2024-03-23 10:59:46 +0800

    Create api/user_in_room endpoint that checkes for room_code session - to see if user was in a previous room

--- backend/music_controller/api/urls.py
+++ backend/music_controller/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create_room', CreateRoomView.as_view()),
     path('get_room', GetRoom.as_view()),
     path('join_room', JoinRoom.as_view()),
+    path('user_in_room', UserInRoom.as_view()),
 ]
