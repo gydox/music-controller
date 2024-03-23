82d00cf51ff19778948e9f5d1dc7691e46ae13f8
Shawn <shawntaydev@gmail.com>
2024-03-23 18:42:56 +0800

    handle leave room

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { Grid, Button, Typography } from "@material-ui/core";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 export default function Room(props) {
-  const defaultVotes = 2;
+  const navigateTo = useNavigate();
+  const defaultVotes = 0;
   const params = useParams();
   const [votesToSkip, setVotesToSkip] = useState(defaultVotes);
   const [guestCanPause, setGuestCanPause] = useState(false);
@@ -18,22 +20,60 @@ export default function Room(props) {
     axios
       .get("http://localhost:8000/api/get_room" + `?code=${params.roomCode}`)
       .then((response) => {
-        const data = response.data; // Access response data directly
+        const data = response.data;
         setVotesToSkip(data.votes_to_skip);
         setGuestCanPause(data.guest_can_pause.toString());
         setIsHost(data.is_host.toString());
       })
       .catch((error) => {
-        console.error("Error fetching room data:", error);
+        // console.error("Error fetching room data:", error);
+        navigateTo("/");
       });
   }, [params.roomCode]);
 
+  const leaveButtonPressed = () => {
+    axios
+      .post("http://localhost:8000/api/leave_room")
+      .then((response) => {
+        const data = response.data;
+        navigateTo("/");
+      })
+      .catch((error) => {
+        console.error("Error leaving room:", error);
+      });
+  };
+
   return (
-    <div>
-      <h3>Room Code: {params.roomCode}</h3>
-      <p>Votes: {votesToSkip}</p>
-      <p>Guest can Pause: {guestCanPause}</p>
-      <p>Host: {isHost}</p>
-    </div>
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h4" component="h4">
+          Room Code: {params.roomCode}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="h6" component="h6">
+          Votes: {votesToSkip}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="h6" component="h6">
+          Guest Can Pause: {guestCanPause.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="h6" component="h6">
+          Is Host: {isHost.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={leaveButtonPressed}
+        >
+          Leave Room
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
