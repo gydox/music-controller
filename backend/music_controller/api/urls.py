1df54e887c1a7e3943a9600d4aadc1f1c909d7c3
Shawn <shawntaydev@gmail.com>
2024-03-23 18:42:12 +0800

    Create endpoint to leave room

--- backend/music_controller/api/urls.py
+++ backend/music_controller/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('get_room', GetRoom.as_view()),
     path('join_room', JoinRoom.as_view()),
     path('user_in_room', UserInRoom.as_view()),
+    path('leave_room', LeaveRoom.as_view()),
 ]
