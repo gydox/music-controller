4c85e6fa34c8187dcc28438e9037f368baffb1a9
Shawn <shawntaydev@gmail.com>
2024-03-24 18:26:43 +0800

    Create update room api endpoint

--- backend/music_controller/api/views.py
+++ backend/music_controller/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, status
-from .serializers import RoomSerializer, CreateRoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer, UpdateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -110,3 +110,38 @@ class LeaveRoom(APIView):
                 room.delete()
 
         return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
+
+
+class UpdateRoom(APIView):
+
+    serializer_class = UpdateRoomSerializer
+
+    def patch(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            code = serializer.data.get('code')
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+
+            rooms = Room.objects.filter(code=code)
+            if not rooms.exists():
+                return Response("Room not found.", status=status.HTTP_404_NOT_FOUND)
+            else:
+                room = rooms[0]
+                user_id = self.request.session.session_key
+                if room.host != user_id:
+                    return Response("You are not the host of this room.", status=status.HTTP_401_UNAUTHORIZED)
+                else:
+                    room.guest_can_pause = guest_can_pause
+                    room.votes_to_skip = votes_to_skip
+                    room.save(
+                        update_fields=[
+                            'guest_can_pause',
+                            'votes_to_skip'
+                        ])
+                    return Response(UpdateRoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
